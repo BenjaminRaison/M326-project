d5e73bac204817a1ca0a853bef66a277a744e538
BenjaminRaison <benjaminraison@rocketmail.com>
2018-06-05 10:57:13 +0200

    Lambdaified exception handler

--- src/main/java/net/baumink/bzz/m326/Launcher.java
+++ src/main/java/net/baumink/bzz/m326/Launcher.java
@@ -1,21 +1,14 @@
 package net.baumink.bzz.m326;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import javax.swing.*;
 
 public class Launcher {
 
     public static void main(String[] args) {
 
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread t, Throwable e) {
-                e.printStackTrace();
-                JOptionPane.showMessageDialog(null, e.getMessage(), "Fehler", JOptionPane.ERROR_MESSAGE);
-            }
+        Thread.setDefaultUncaughtExceptionHandler((t, e) -> {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, e.getMessage(), "Fehler", JOptionPane.ERROR_MESSAGE);
         });
-
-        throw new RuntimeException("NOT IMPLEMENTED");
     }
 }
