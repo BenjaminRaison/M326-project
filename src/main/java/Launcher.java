2bd93dfae4976d954394e8f73a32c66c4ebc9a84
JonasGredig <jongredig@gmail.com>
2018-06-05 09:27:05 +0200

    added UncaughtExceptionHandling

---
+++ src/main/java/Launcher.java
@@ -0,0 +1,18 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.swing.*;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                JOptionPane.showMessageDialog(null, e.getMessage(), "Fehler", JOptionPane.ERROR_MESSAGE);
+            }
+        });
+
+        throw new RuntimeException("NOT IMPLEMENTED");
+    }
+}
