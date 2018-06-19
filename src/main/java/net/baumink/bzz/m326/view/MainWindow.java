590d33a486fbf4ca7ee89c79dcb0cf24031e9a4d
JonasGredig <jongredig@gmail.com>
2018-06-19 09:22:39 +0200

    added basic JTable

--- src/main/java/net/baumink/bzz/m326/view/MainWindow.java
+++ src/main/java/net/baumink/bzz/m326/view/MainWindow.java
@@ -5,9 +5,12 @@ import net.baumink.bzz.m326.db.dao.impl.EmployeeDaoImpl;
 import net.baumink.bzz.m326.db.pojo.Employee;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
 
 public class MainWindow extends JFrame {
@@ -17,10 +20,13 @@ public class MainWindow extends JFrame {
     public MainWindow() {
         super();
         setTitle("M236");
-        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
         setSize(new Dimension(800, 500));
 
         JPanel functionalityPanel = new JPanel();
+        FlowLayout functionalityPanelLayout = new FlowLayout();
+        functionalityPanelLayout.setAlignment(FlowLayout.RIGHT);
+        functionalityPanel.setLayout(functionalityPanelLayout);
 
         EmployeeDaoImpl employeeDao = new EmployeeDaoImpl(DBConnection.getConnection());
         java.util.List<Employee> list = employeeDao.getAll();
@@ -32,10 +38,24 @@ public class MainWindow extends JFrame {
         JComboBox<Employee> employeeBox = new JComboBox<>(employees);
         JComboBox<String> departmentBox = new JComboBox<>(departments);
 
+
+        Vector<String> headerColumn = new Vector<>();
+        headerColumn.addElement("ID");
+        headerColumn.addElement("Client");
+        headerColumn.addElement("Status");
+        headerColumn.addElement("Delivery planned");
+        headerColumn.addElement("Last Edited by");
+        headerColumn.addElement("");
+
+        DefaultTableModel tblModel = new DefaultTableModel(2, 2);
+        tblModel.setColumnIdentifiers(headerColumn);
+
+        JTable table = new JTable(tblModel);
+
         functionalityPanel.add(employeeBox);
         functionalityPanel.add(departmentBox);
-        add(functionalityPanel);
-        add(contentPane);
+        add(functionalityPanel, BorderLayout.NORTH);
+        add(new JScrollPane(table), BorderLayout.CENTER);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
 
