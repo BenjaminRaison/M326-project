78c6a9db76b0acc0f2a78cc18d020d3bc024a940
JonasGredig <jongredig@gmail.com>
2018-06-28 09:29:23 +0200

    add Actionlistener

--- src/main/java/net/baumink/bzz/m326/view/MainWindow.java
+++ src/main/java/net/baumink/bzz/m326/view/MainWindow.java
@@ -7,6 +7,8 @@ import net.baumink.bzz.m326.db.DBConnection;
 import net.baumink.bzz.m326.db.pojo.Employee;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
@@ -79,7 +81,12 @@ public class MainWindow extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         comboEmployee.addActionListener(e -> updateData());
-
+        table.getModel().addTableModelListener(new TableModelListener() {
+            @Override
+            public void tableChanged(TableModelEvent e) {
+                //TODO
+            }
+        });
         setVisible(true);
     }
 
